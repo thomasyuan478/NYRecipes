34870224bd786fb85ce552009100246dbde1ca86
Thomas Yuan <thomasyuan478@gmail.com>
2023-09-13 11:28:09 -0500

    started update feature

--- react-app/src/components/ReviewComponent/index.js
+++ react-app/src/components/ReviewComponent/index.js
@@ -1,26 +1,35 @@
 import { useDispatch } from "react-redux";
 import { deleteReviewThunk } from "../../store/recipe";
+import { useState } from "react";
 
 export const ReviewComponent = ({ review }) => {
   const dispatch = useDispatch();
+  const [update, setUpdate] = useState(false);
 
   const deleteButton = (e) => {
     console.log(review.id);
     dispatch(deleteReviewThunk(review.id));
   };
 
+  const toggleUpdate = () => {
+    setUpdate(true);
+    if (update === true) setUpdate(false);
+  };
+
   return (
     <>
       <div>
         <div>
-          {review.comment} RATING - {review.star_rating}
+          {update && <textarea value={review.comment}></textarea>}
+          {!update && review.comment} RATING - {review.star_rating}
         </div>
         <div>
           {review.user.firstName} {review.user.lastName} // ReviewId-#
           {review.id}
         </div>
-        <button> Future Update </button>
-        <button onClick={deleteButton}> Future Delete </button>
+        {!update && <button onClick={toggleUpdate}> Future Update </button>}
+        {update && <button onClick={toggleUpdate}> Future Cancel </button>}
+        <button onClick={deleteButton}> Delete </button>
       </div>
     </>
   );
