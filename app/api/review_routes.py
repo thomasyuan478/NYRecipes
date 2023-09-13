5135ca9c9a1ac61d2f048093dc229a69fddd5067
Thomas Yuan <thomasyuan478@gmail.com>
2023-09-13 13:37:08 -0500

    update signup form, update review feature

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify, request
 from flask_login import login_required
 from app.models import Recipe, Review
-from app.forms import RecipeForm
+from app.forms import RecipeForm, ReviewForm
 from app import db
 
 review_routes = Blueprint("reviews", __name__)
@@ -18,3 +18,27 @@ def delete_review(id):
 
   else:
     return {"error": "Review could not be found"}
+
+@review_routes.route("/<int:id>", methods=["PUT"])
+def update_review(id):
+  form = ReviewForm()
+
+  review = Review.query.get(id)
+
+  form['csrf_token'].data = request.cookies['csrf_token']
+
+  recipe = Recipe.query.get(review.recipe_id)
+
+
+  if not review:
+    return {"error": "Review could not be found"}
+
+  if form.validate_on_submit():
+    print(form.data)
+    review.comment = form.data["comment"]
+    review.star_rating = form.data["star_rating"]
+    db.session.commit()
+    return {"recipe": recipe.to_dict_detailed()}
+
+  else:
+    return {"error": "something went wrong"}
--- react-app/src/components/Recipes/index.js
+++ react-app/src/components/Recipes/index.js
@@ -2,9 +2,11 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getRecipesThunk } from "../../store/recipe";
 import { RecipeCard } from "../RecipeCard";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 export const Recipes = () => {
   const dispatch = useDispatch();
+  const history = useHistory();
 
   useEffect(() => {
     dispatch(getRecipesThunk());
@@ -17,7 +19,10 @@ export const Recipes = () => {
 
   return (
     <>
-      <h1>Hello There</h1>
+      <h1>Recipes Page</h1>
+      <button onClick={(e) => history.push("/new")}>
+        Click here if you would like to add a recipe
+      </button>
       {ids.map((id) => (
         <div>
           <RecipeCard key={id} recipe={smallRecipes[id]} />
